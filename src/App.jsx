580d4c1814a0ee3b1ea4896e236977d9154e8827
jwojszczyk <jwojszczyk@gmail.com>
2025-09-17 12:39:37 +0200

    working logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+// import innych stron, jeśli będą np. About, Contact
+// import About from "./pages/About";
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* inne strony można dodać w ten sposób */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Logo_web.jsx
+++ src/components/Logo_web.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../Pictures/Logo_banner.png";
 
 const Logo_web = () => {
   return (
-    <div
-      style={{
-        position: "absolute",
-        width: "188px",
-        height: "208px",
-        marginLeft: "36px",
-        backgroundImage: `url(${logo})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        overflow: "hidden",
-        zIndex: "10",
-      }}
-    ></div>
+    <Link to="/">
+      <div
+        style={{
+          position: "absolute",
+          width: "188px",
+          height: "208px",
+          marginLeft: "36px",
+          backgroundImage: `url(${logo})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          overflow: "hidden",
+          zIndex: "10",
+        }}
+      ></div>
+    </Link>
   );
 };
 export default Logo_web;
